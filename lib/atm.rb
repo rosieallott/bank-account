4e1765e47a326d45fdd7d7857d5e844a7c398ca4
Rosie Allott <rosie.allott@gmail.com>
2016-10-18 13:37:34 +0100

    ATM class with view_statement - need to fix test doubles

--- lib/atm.rb
+++ lib/atm.rb
@@ -2,4 +2,13 @@ require_relative 'account'
 require_relative 'statement'
 
 class Atm
-end 
+
+  def initialize(statement_class = Statement)
+    @statement_class = statement_class
+  end
+
+  def view_statement(account)
+    statement = @statement_class.new(account)
+    statement.display
+  end
+end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,5 +1,19 @@
 require 'atm'
 
 describe Atm do
-  subject(:atm) {described_class.new}
-end 
+  subject(:atm) {described_class.new(statement_class)}
+  let(:statement_class) {double :statement_class, new: statement}
+  let(:statement) {double :statement, display: nil}
+  let(:account) {double :account}
+
+  describe "#view_statement" do
+    it "should instantiate Statement class" do
+      atm.view_statement(account)
+      expect(statement_class).to receive(:new)
+    end
+    it "should call display on the instance of Statement" do
+      atm.view_statement(account)
+      expect(statement).to receive(:display)
+    end
+  end
+end
