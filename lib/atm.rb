ccdadec3b489ac2603b54cc76a0e6713104786d5
Rosie Allott <rosie.allott@gmail.com>
2016-10-18 13:56:05 +0100

    #withdraw and #deposit in ATM - need to fix failing doubles

--- lib/atm.rb
+++ lib/atm.rb
@@ -3,12 +3,22 @@ require_relative 'statement'
 
 class Atm
 
-  def initialize(statement_class = Statement)
+  def initialize(account, statement_class = Statement)
+    @account = account
     @statement_class = statement_class
   end
 
-  def view_statement(account)
-    statement = @statement_class.new(account)
+  def view_statement
+    statement = @statement_class.new(@account)
     statement.display
   end
+
+  def deposit(amount)
+    @account.store_transaction(:credit, amount)
+  end
+
+  def withdraw(amount)
+    @account.store_transaction(:debit, amount)
+  end
+
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,19 +1,32 @@
 require 'atm'
 
 describe Atm do
-  subject(:atm) {described_class.new(statement_class)}
+  subject(:atm) {described_class.new(account, statement_class)}
   let(:statement_class) {double :statement_class, new: statement}
   let(:statement) {double :statement, display: nil}
-  let(:account) {double :account}
+  let(:account) {double :account, store_transaction: nil, retrieve_transactions: nil}
 
   describe "#view_statement" do
     it "should instantiate Statement class" do
-      atm.view_statement(account)
+      atm.view_statement
       expect(statement_class).to receive(:new)
     end
     it "should call display on the instance of Statement" do
-      atm.view_statement(account)
+      atm.view_statement
       expect(statement).to receive(:display)
+    end
+  end
+
+  describe "#deposit and #withdraw" do
+    it "deposit calls store on account with :credit argument" do
+      allow(account).to receive(:store_transaction).with(:credit, 100)
+      atm.deposit(100)
+      expect(account).to receive(:store_transaction).with(:credit, 100)
+    end
+    it "deposit calls store on account with :debit argument" do
+      allow(account).to receive(:store_transaction).with(:debit, 100)
+      atm.withdraw(100)
+      expect(account).to receive(:store_transaction).with(:debit, 100)
     end
   end
 end
